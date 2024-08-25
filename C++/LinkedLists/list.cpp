1004a90a28f072cba2becd38e619facbf33f2882
DymNomZ <dymierborgonia@gmail.com>
2024-08-25 13:30:46 +0800

    Fully working queue

--- C++/LinkedLists/list.cpp
+++ C++/LinkedLists/list.cpp
@@ -17,89 +17,105 @@ int List::find_element(int e){
             count++;
             ptr = ptr->next;
         }
-        free(ptr);
-        return e;
+        //free(ptr);
+        return count;
     }
 }
 
 void List::add(int e){
     if(head == NULL){
         head = (Node*)calloc(1, sizeof(Node));
-        tail = (Node*)calloc(1, sizeof(Node));
         head->data = e;
         head->next = NULL;
-        tail = head;
-        //cout << head->data << endl;
+    }
+    else if(tail == NULL){
+        tail = (Node*)calloc(1, sizeof(Node));
+        tail->data = e;
+        tail->next = NULL;
+        head->next = tail;
     }
     else{
-        Node* pck = (Node*)calloc(1, sizeof(Node));
-        pck->data = e;
-        pck->next = NULL;
-        tail->next = pck;
-        tail = pck;
+        tail->next = (Node*)calloc(1, sizeof(Node));
+        tail->next->data = e;
+        tail->next->next = NULL;
+        tail = tail->next;
     }
 }
 
 Node* List::subtract(){
     Node* pck = (Node*)calloc(1, sizeof(Node));
+    pck->data = -1;
+    pck->next = NULL;
+    Node* ptr = head;
     if(tail == NULL){
         cout << "List is empty, returning -1..." << endl;
-        pck->data = -1;
         return pck;
     }
-    if(tail->next == NULL){
-        pck->data = tail->data;
-        tail = NULL;
-        head = NULL;
-        return pck;
-    }
-    else{
-        Node* ptr = head;
-        while(ptr->next != tail){
+    else if(ptr->next != NULL){
+        while(ptr->next != tail){ //check if ptr is not the only node
             ptr = ptr->next;
         }
         pck = ptr->next;
-        //free(ptr->next);
         ptr->next = NULL;
         tail = ptr;
-        //free(ptr);
+        return pck;
+    }
+    else{
+        pck = tail;
+        tail = NULL;
+        head = NULL;
         return pck;
     }
 }
 
 Node* List::remove(int e){
     Node* pck = (Node*)calloc(1, sizeof(Node));
+    pck->data = -1;
+    pck->next = NULL;
+    Node* ptr = head;
     if(head == NULL){
         cout << "List is empty, returning -1..." << endl;
-        pck->data = -1;
         return pck;
-    }else{
-        Node* ptr = head;
-        while(ptr->next->data != e){
+    }else if(ptr->data != e){
+        while(ptr->next->data != e && ptr->next != NULL){
             ptr = ptr->next;
         }
-        pck = ptr->next;
-        ptr->next = NULL;
-        ptr->next = pck->next;
-        
-        //free(ptr);
+        if(ptr->next == NULL){
+            cout << "Element not found, returning -1..." << endl;
+            return pck;
+        }else{
+            pck = ptr->next;
+            ptr->next = ptr->next->next;
+            if(ptr->next == NULL) tail = ptr;
+            return pck;
+        }
+    }else{
+        pck = head;
+        head = head->next;
+        if(head == NULL) tail = NULL;
         return pck;
     }
 }
 
 void List::insert(int e, int n){
+    Node* pck = (Node*)calloc(1, sizeof(Node));
+    pck->data = -1;
+    pck->next = NULL;
+    Node* ptr = head;
     if(head == NULL) cout << "List is empty" << endl;
+    else if(n == 0) append_start(e);
     else{
-        int count = 0;
-        Node* ptr = head;
-        while(count != n-1 && ptr->next != NULL){
+        int count = 1;
+        while(count <= n-1 && ptr != NULL){
             ptr = ptr->next;
+            count++;
         }
-        Node* pck = (Node*)calloc(1, sizeof(Node));
-        pck->data = e;
-        pck->next = ptr->next;
-        ptr->next = pck;
-        free(ptr);
+        if(ptr == NULL) cout << "Invalid idx" << endl;
+        else{
+            pck->data = e;
+            pck->next = ptr->next;
+            ptr->next = pck;
+        }
     }
 }
 
@@ -116,9 +132,11 @@ void List::append_start(int e){
 void List::remove_start(){
     if(head == NULL) cout << "List is empty" << endl;
     else{
-        Node* ptr = head;
-        head = head->next;
-        free(ptr);
+        if(head->next != NULL)head = head->next;
+        else{
+            head = NULL;
+            tail = NULL;
+        }
     }
 }
 
